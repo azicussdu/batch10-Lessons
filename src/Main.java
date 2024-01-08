175d72916829796228f3a37b94885ab055767a3b
Azamat Tolegenov <azicus.sdu@gmail.com>
2024-01-08 18:44:03 +0600

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Project begins here");
         System.out.println("Second change");
+        System.out.println("Third change");
     }
 }
